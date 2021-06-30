dce20f0e433f75b8c0be8165d055ee6eb62981ee
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2021-07-01 03:03:43 +0430

    Enable A20 Line

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -67,6 +67,12 @@ load32:
     mov ss, ax
     mov ebp, 0x00200000
     mov esp, ebp
+
+    ; Enable A20 Line
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
     jmp $
 
 times 510 - ($ - $$) db 0
