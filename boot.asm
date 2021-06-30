32fb7c62b3e33995b9c0d5aa16da8b5c7f71aa66
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2021-07-01 02:37:33 +0430

    Add other segments initilization after entering protected mode

--- boot.asm
+++ boot.asm
@@ -59,6 +59,14 @@ gdt_descriptor:
 
 [BITS 32]
 load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
 
 times 510 - ($ - $$) db 0
