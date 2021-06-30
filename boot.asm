a32859268f42bc7cce1c75ed8c5edc276578e7d7
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2021-06-30 23:33:26 +0430

    Remove old interrupt and printing codes

--- boot.asm
+++ boot.asm
@@ -10,18 +10,6 @@ times 33 db 0
 start:
     jmp 0x7c0:step2
 
-handle_zero:
-    mov ah, 0eh
-    mov al, '0'
-    int 10h
-    ret
-
-handle_one:
-    mov ah, 0eh
-    mov al, '1'
-    int 10h
-    ret
-
 step2:
     cli
     mov ax, 0x7c0
@@ -32,16 +20,6 @@ step2:
     mov sp, 0x7c00
     sti
 
-    mov word [ss:0x00], handle_zero
-    mov word [ss:0x02], 0x7c0
-
-    mov word [ss:0x04], handle_one
-    mov word [ss:0x06], 0x7c0
-
-    int 0x0
-    int 0x1
-    mov si, message
-    call print
     jmp $
 
 print:
@@ -59,8 +37,6 @@ print_char:
     mov ah, 0eh
     int 10h
     ret
-
-message: db 'Hello World!', 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
